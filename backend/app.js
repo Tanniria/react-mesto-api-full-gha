cb283d19831f4d1fd0c27aa8dd5a02764fe5775b
Tatiana Smakova <tensmakova@gmail.com>
2023-07-11 17:39:28 +0500

    15 пр

--- backend/app.js
+++ backend/app.js
@@ -15,6 +15,7 @@ const cors = require('./middlewares/cors');
 
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 const app = express();
+
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 100,
