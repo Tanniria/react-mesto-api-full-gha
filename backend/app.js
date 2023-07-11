3eb8290cefe92def0d16a41a1854e7c3594e4171
Tatiana Smakova <tensmakova@gmail.com>
2023-07-11 17:07:45 +0500

    15

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ const { createUser, login } = require('./controllers/users');
 const { auth } = require('./middlewares/auth');
 const { validatesignin, validatesignup } = require('./middlewares/validate');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-const cors = require('./middlewares/cors');
+// const cors = require('./middlewares/cors');
 
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 const app = express();
@@ -26,7 +26,7 @@ app.use(express.json());
 app.use(requestLogger);
 app.use(limiter);
 app.use(helmet());
-app.use(cors);
+// app.use(cors);
 
 mongoose.connect(DB_URL);
 
